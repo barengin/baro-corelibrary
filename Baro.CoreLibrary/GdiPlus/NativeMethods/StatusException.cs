59f2ba067d5ef8f1f3216a6347b33f5b5222ae48
baris.engin@gmail.com <baris.engin@gmail.com@26701070-cfd0-cbc1-eb93-371242429e4a>
2012-07-15 16:25:05 +0000

    GdiPlus added.

---
+++ Baro.CoreLibrary/GdiPlus/NativeMethods/StatusException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Baro.CoreLibrary.GdiPlus
+{
+    internal class GdiPlusStatusException:Exception
+    {
+        GdiPlusStatusException(GpStatus status, string msg)
+            : base(msg)
+        {
+            Status = status;
+        }
+
+        public GpStatus Status { get; private set; }
+
+        public static Exception Exception(GpStatus status)
+        {
+            return new GdiPlusStatusException(status,status.ToString());
+        }
+    }
+
+}
