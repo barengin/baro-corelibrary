9871b6029c17de2284015c988abeeeff714c81a5
baris.engin@gmail.com <baris.engin@gmail.com@26701070-cfd0-cbc1-eb93-371242429e4a>
2012-07-15 16:25:05 +0000

    GdiPlus added.

---
+++ trunk/Baro.CoreLibrary/GdiPlus/NativeMethods/Fonts.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Baro.CoreLibrary.GdiPlus
+{
+    internal partial class GdiPlus
+    {
+        //----------------------------------------------------------------------------
+        // Font APIs
+        //----------------------------------------------------------------------------
+
+
+    }
+}
