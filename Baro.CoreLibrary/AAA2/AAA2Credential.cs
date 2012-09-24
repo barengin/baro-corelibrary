0f9972ad56c4468abbaaa34a0207d2eb677e56b1
baris.engin@gmail.com <baris.engin@gmail.com@26701070-cfd0-cbc1-eb93-371242429e4a>
2012-09-24 05:50:56 +0000

    AAA2 eklendi.

---
+++ Baro.CoreLibrary/AAA2/AAA2Credential.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Baro.CoreLibrary.AAA2
+{
+    [DataContract]
+    public struct AAA2Credential
+    {
+        private string _usr, _pwd;
+
+        [DataMember]
+        public string Password
+        {
+            set { _pwd = value; }
+            get { return _pwd; }
+        }
+
+        [DataMember]
+        public string Username
+        {
+            set { _usr = value; }
+            get { return _usr; }
+        }
+
+        public AAA2Credential(string usr, string pwd)
+        {
+            _usr = usr;
+            _pwd = pwd;
+        }
+    }
+}
---
+++ Baro.CoreLibrary/AAA2/Key.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Concurrent;
+
+namespace Baro.CoreLibrary.AAA2
+{
+    sealed class Key
+    {
+        private ConcurrentDictionary<string, User2> _keys = new ConcurrentDictionary<string, User2>(Environment.ProcessorCount, 20000);
+
+        public bool Add(string value, User2 user)
+        {
+            return _keys.TryAdd(value, user);
+        }
+
+        public bool Get(string value, out User2 user)
+        {
+            return _keys.TryGetValue(value, out user);
+        }
+    }
+}
