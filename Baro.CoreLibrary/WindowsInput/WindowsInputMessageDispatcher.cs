2dd47855d37110ca8b9cdc608865feda8ca62c13
baris.engin@gmail.com <baris.engin@gmail.com@26701070-cfd0-cbc1-eb93-371242429e4a>
2012-10-15 15:37:58 +0000

    WindowsInput

---
+++ Baro.CoreLibrary/WindowsInput/WindowsInputMessageDispatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Baro.CoreLibrary.WindowsInput.Native;
+
+namespace Baro.CoreLibrary.WindowsInput
+{
+    /// <summary>
+    /// Implements the <see cref="IInputMessageDispatcher"/> by calling <see cref="NativeMethods.SendInput"/>.
+    /// </summary>
+    internal class WindowsInputMessageDispatcher : IInputMessageDispatcher
+    {
+        /// <summary>
+        /// Dispatches the specified list of <see cref="INPUT"/> messages in their specified order by issuing a single called to <see cref="NativeMethods.SendInput"/>.
+        /// </summary>
+        /// <param name="inputs">The list of <see cref="INPUT"/> messages to be dispatched.</param>
+        /// <returns>
+        /// The number of <see cref="INPUT"/> messages that were successfully dispatched.
+        /// </returns>
+        public UInt32 DispatchInput(INPUT[] inputs)
+        {
+            return WINativeMethods.SendInput((UInt32)inputs.Count(), inputs.ToArray(), Marshal.SizeOf(typeof (INPUT)));
+        }
+    }
+}
