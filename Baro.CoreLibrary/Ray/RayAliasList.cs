4f8ef8d0925f9c441b3ee8211b5da5af7a0ed3ff
baris.engin@gmail.com <baris.engin@gmail.com@26701070-cfd0-cbc1-eb93-371242429e4a>
2013-03-27 09:02:39 +0000

    TS

--- Baro.CoreLibrary/Ray/RayAliasList.cs
+++ Baro.CoreLibrary/Ray/RayAliasList.cs
@@ -2,77 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace Baro.CoreLibrary.Ray
 {
-    public class RayAliasList: RayItem<RayAliasList>, IList<string>
+    public class RayAliasList : RayItem<RayAliasList>, IList<string>
     {
-        private List<string> _list = new List<string>();
+        private List<string> _flylist = null;
+
+        private List<string> _list
+        {
+            get { return _flylist ?? (_flylist = new List<string>()); }
+        }
 
         public override RayAliasList Clone()
         {
             RayAliasList a = new RayAliasList();
 
-            foreach (var item in _list)
-            {
-                a._list.Add(item);
-            }
+            ReaderLock(() =>
+                {
+                    foreach (var item in _list)
+                    {
+                        a._list.Add(item);
+                    }
+                }
+            );
 
             return a;
         }
 
         public int IndexOf(string item)
         {
-            return _list.IndexOf(item);
+            return ReaderLock<int>(() => _list.IndexOf(item));
         }
 
         public void Insert(int index, string item)
         {
-            _list.Insert(index, item);
+            WriterLock(() => _list.Insert(index, item));
         }
 
         public void RemoveAt(int index)
         {
-            _list.RemoveAt(index);
+            WriterLock(() => _list.RemoveAt(index));
         }
 
         public string this[int index]
         {
             get
             {
-                return _list[index];
+                return ReaderLock<string>(() => _list[index]);
             }
             set
             {
-                _list[index] = value;
+                WriterLock(() => _list[index] = value);
             }
         }
 
         public void Add(string item)
         {
-            _list.Add(item);
+            WriterLock(() => _list.Add(item));
         }
 
         public void Clear()
         {
-            _list.Clear();
+            WriterLock(() => _list.Clear());
         }
 
         public bool Contains(string item)
         {
-            return _list.Contains(item);
+            return ReaderLock<bool>(() => _list.Contains(item));
         }
 
         public void CopyTo(string[] array, int arrayIndex)
         {
-            _list.CopyTo(array, arrayIndex);
+            ReaderLock(() => _list.CopyTo(array, arrayIndex));
         }
 
         public int Count
         {
-            get { return _list.Count; }
+            get
+            {
+                return ReaderLock<int>(() => _list.Count);
+            }
         }
 
         public bool IsReadOnly
@@ -82,31 +95,34 @@ namespace Baro.CoreLibrary.Ray
 
         public bool Remove(string item)
         {
-            return _list.Remove(item);
+            return WriterLock<bool>(() => _list.Remove(item));
         }
 
         public IEnumerator<string> GetEnumerator()
         {
-            return _list.GetEnumerator();
+            return ReaderLock<IEnumerator<string>>(() => _list.GetEnumerator());
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            return _list.GetEnumerator();
+            return ReaderLock<System.Collections.IEnumerator>(() => _list.GetEnumerator());
         }
 
         public override XmlNode CreateXmlNode(XmlDocument xmlDoc)
         {
             XmlNode n = xmlDoc.CreateElement("aliases");
 
-            foreach (var item in _list)
-            {
-                XmlNode a = xmlDoc.CreateElement("a");
-                
-                XmlAttribute t = xmlDoc.CreateAttribute("name");
-                t.Value = item;
-                n.AppendChild(a);
-            }
+            ReaderLock(() =>
+                {
+                    foreach (var item in _list)
+                    {
+                        XmlNode a = xmlDoc.CreateElement("a");
+
+                        XmlAttribute t = xmlDoc.CreateAttribute("name");
+                        t.Value = item;
+                        n.AppendChild(a);
+                    }
+                });
 
             return n;
         }
